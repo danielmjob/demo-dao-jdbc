ebe167928ff0716251d334ef1984672a5abdb4ad
danielmjob <danielmarquesjob@gmail.com>
2023-03-08 10:30:30 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -93,7 +96,7 @@ public class SellerDaoJDBC implements SellerDao{
       
       Department dep = new Department();
       dep.setId(rs.getInt("DepartmentId"));
-      dep.setName(rs.getString("Name"));
+      dep.setName(rs.getString("DepName")); // CORREÇÃO --> Name para DepName 
 
       return dep;
       
@@ -103,6 +106,57 @@ public class SellerDaoJDBC implements SellerDao{
     public List<Seller> findAll() {
        return null;
     }
+
+    // ficou parecido com o findById com algumas particularidades
+   @Override
+   public List<Seller> findByDepartment(Department department) {
+      PreparedStatement st = null;
+      ResultSet rs = null;
+
+      try{
+
+         st = conn.prepareStatement(
+           "SELECT seller.*,department.Name as DepName "
+         + "FROM seller INNER JOIN department "
+         + "ON seller.DepartmentId = department.Id "
+         + "WHERE DepartmentId = ? "
+         + "ORDER BY Name ");
+
+         st.setInt(1, department.getId());
+
+         rs = st.executeQuery();
+
+         List<Seller> list = new ArrayList<>();
+         Map<Integer, Department> map = new HashMap<>(); // criado um map vazio
+
+         while(rs.next()){
+
+            Department dep = map.get(rs.getInt("DepartmentId"));
+
+            // so instancia um departamento se ele ainda não existir
+            if (dep == null){
+               dep = instantiateDepartment(rs);
+               map.put(rs.getInt("DepartmentId"), dep); // joga o departmento no map
+            }
+
+            // feito dessa forma para que seja criado um departamento com varios vendedores e não um departamento por vendedor, gerando repetições de departamento
+
+            Seller obj = instantiateSeller(rs, dep);
+            list.add(obj);
+
+         }
+         return list;
+
+
+      }catch(SQLException e){
+         throw new DbException(e.getMessage());
+
+      }finally{
+         DB.closeStatement(st);
+         DB.closeResultSet(rs);
+
+      }
+   }
 
 
     
